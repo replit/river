84f8ea0611d61c76ca9e42395125eeaf05854baf
Jacky Zhao <j.zhao2k19@gmail.com>
2023-09-12 15:57:44 -0700

    version bump to 0.1.2, fix exports

---
+++ index.ts
@@ -0,0 +1,47 @@
+export { serializeService, ServiceBuilder } from './router/builder';
+export type {
+  ValidProcType,
+  ProcListing,
+  Service,
+  ProcHandler,
+  ProcInput,
+  ProcOutput,
+  ProcType,
+  Procedure,
+} from './router/builder';
+
+export { createClient } from './router/client';
+export type { ServerClient } from './router/client';
+
+export { createServer } from './router/server';
+export { asClientRpc, asClientStream } from './router/server.util';
+export type { Server } from './router/server';
+
+export { Transport } from './transport/types';
+export {
+  TransportMessageSchema,
+  OpaqueTransportMessageSchema,
+  TransportAckSchema,
+  msg,
+  payloadToTransportMessage,
+  ack,
+  reply,
+} from './transport/message';
+export type {
+  TransportMessage,
+  MessageId,
+  OpaqueTransportMessage,
+  TransportClientId,
+  TransportMessageAck,
+} from './transport/message';
+
+export { StdioTransport } from './transport/stdio';
+export { WebSocketTransport } from './transport/ws';
+export {
+  createWebSocketServer,
+  onServerReady,
+  createWsTransports,
+  waitForMessage,
+  waitForSocketReady,
+  createWebSocketClient,
+} from './transport/ws.util';
