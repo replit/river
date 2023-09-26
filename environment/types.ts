f92fa3d2f5e7fc218a6cc90cb66508428685440b
Bri <34875062+Monkatraz@users.noreply.github.com>
2023-09-26 13:14:42 -0600

    add declaration merging comment

--- environment/types.ts
+++ environment/types.ts
@@ -1,4 +1,16 @@
-export interface IsomorphicEnvironment {
-  log: (...args: unknown[]) => void;
-  error: (...args: unknown[]) => void;
-}
+/**
+ * The environment for services/procedures. This is used only on
+ * the server.
+ *
+ * You should use declaration merging to extend this interface
+ * with whatever you need. For example, if you need to access
+ * a database, you can do:
+ * ```ts
+ * declare module '@replit/river' {
+ *   interface IsomorphicEnvironment {
+ *     db: Database;
+ *   }
+ * }
+ * ```
+ */
+export interface IsomorphicEnvironment {}
