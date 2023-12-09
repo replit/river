12e3756e0f13f29ea6dd1b5dcbf250a4235d5d32
Jacky Zhao <j.zhao2k19@gmail.com>
2023-12-08 21:48:11 -0800

    better test utils to wait for a value to be equal, add invariant tests

--- __tests__/fixtures/cleanup.ts
+++ __tests__/fixtures/cleanup.ts
@@ -2,6 +2,11 @@ import { expect, vi } from 'vitest';
 import { Connection, Transport } from '../../transport';
 import { Server } from '../../router';
 
+const waitUntilOptions = {
+  timeout: 250, // these are all local connections so anything above 250ms is sus
+  interval: 5, // check every 5ms
+};
+
 export async function ensureTransportIsClean(t: Transport<Connection>) {
   expect(
     t.state,
@@ -15,17 +20,42 @@ export async function ensureTransportIsClean(t: Transport<Connection>) {
     t.messageHandlers,
     `transport ${t.clientId} should not have open message handlers after the test`,
   ).toStrictEqual(new Set());
+}
 
-  vi.waitUntil(() => t.sendQueue.size === 0).finally(() => {
-    expect(
-      t.sendQueue,
-      `transport ${t.clientId} should not have any messages waiting to send after the test`,
-    ).toStrictEqual(new Map());
-  });
+export async function waitUntil<T>(
+  valueGetter: () => T,
+  expected: T,
+  message?: string,
+) {
+  return vi
+    .waitUntil(() => valueGetter() === expected, waitUntilOptions)
+    .finally(() => {
+      expect(valueGetter(), message).toEqual(expected);
+    });
+}
+
+export async function ensureTransportQueuesAreEventuallyEmpty(
+  t: Transport<Connection>,
+) {
+  await waitUntil(
+    () => t.sendQueue.size,
+    0,
+    `transport ${t.clientId} should not have any messages waiting to send after the test`,
+  );
+
+  await waitUntil(
+    () => t.sendBuffer.size,
+    0,
+    `transport ${t.clientId} should not have any un-acked messages after the test`,
+  );
 }
 
 export async function ensureServerIsClean(s: Server<unknown>) {
-  return vi.waitUntil(() => s.streams.size === 0);
+  return waitUntil(
+    () => s.streams.size,
+    0,
+    `server should not have any open streams after the test`,
+  );
 }
 
 export async function testFinishesCleanly({
