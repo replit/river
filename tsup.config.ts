4c07de75334065f807c192fbfd11175d017de8d9
Jacky Zhao <j.zhao2k19@gmail.com>
2023-12-15 11:04:30 -0800

    base tsup setup

---
+++ tsup.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'tsup';
+
+export default defineConfig({
+  entry: [
+    'router/index.ts',
+    'logging/index.ts',
+    'codec/index.ts',
+    'util/testHelpers.ts',
+    'transport/index.ts',
+    'transport/impls/ws/client.ts',
+    'transport/impls/ws/server.ts',
+    'transport/stdio.ts',
+  ],
+  format: ['esm', 'cjs'],
+  splitting: false,
+  sourcemap: true,
+  clean: true,
+  dts: true,
+});
