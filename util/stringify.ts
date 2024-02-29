8998030b5e7aa424bb48f7fef5bc1149d72d43f2
Jacky Zhao <j.zhao2k19@gmail.com>
2024-02-29 15:20:43 -0800

    coerce error string properly

--- util/stringify.ts
+++ util/stringify.ts
@@ -1,6 +1,6 @@
 export function coerceErrorString(err: unknown): string {
   if (err instanceof Error) {
-    return `${err.name} (${err.message})`;
+    return err.message;
   }
 
   return `[coerced to error] ${String(err)}`;
