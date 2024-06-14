f7d6cae46d9d599ca95a2563aeb50f456866df24
Jacky Zhao <j.zhao2k19@gmail.com>
2024-06-14 09:55:42 -0700

    see if test reporter works?

--- vitest.config.ts
+++ vitest.config.ts
@@ -10,6 +10,10 @@ export default defineConfig({
     sequence: {
       hooks: 'stack',
     },
+    reporters: process.env.GITHUB_ACTIONS
+      ? ['basic', 'github-actions', 'junit']
+      : ['default'],
+    outputFile: 'test-results.xml',
     pool: 'forks',
     testTimeout: 1000,
     setupFiles: './__tests__/globalSetup.ts',
