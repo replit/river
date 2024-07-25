5cb0ede5e0b33bc9d2e56557a6abdf1a17a3ae7c
Faris Masad <farismasad@gmail.com>
2024-07-25 00:23:00 +0000

    Expose serializeSchemaBackwardsCompatible

--- router/index.ts
+++ router/index.ts
@@ -11,6 +11,7 @@ export type {
 export {
   ServiceSchema,
   serializeSchema,
+  serializeSchemaBackwardsCompatible,
   SerializedServerSchema,
   SerializedServiceSchema,
   SerializedProcedureSchema,
