b2d74549b00cde909225863a90d4794794e3fea6
Jacky Zhao <j.zhao2k19@gmail.com>
2025-11-17 12:36:13 -0800

    use msgpackr instead of @msgpack/msgpack (#350)
    ## Why
    
    its fast!
    
    ## What changed
    
    use msgpackr instead of @msgpack/msgpack
    
    ## Versioning
    
    - [ ] Breaking protocol change
    - [ ] Breaking ts/js API change
    
    <!-- Kind reminder to add tests and updated documentation if needed -->

--- codec/binary.ts
+++ codec/binary.ts
@@ -1,44 +1,25 @@
-import { DecodeError, ExtensionCodec, decode, encode } from '@msgpack/msgpack';
+import { Options, Packr } from 'msgpackr';
 import { Codec } from './types';
 
-const BIGINT_EXT_TYPE = 0;
-const extensionCodec = new ExtensionCodec();
-extensionCodec.register({
-  type: BIGINT_EXT_TYPE,
-  encode(input: unknown): Uint8Array | null {
-    if (typeof input === 'bigint') {
-      if (
-        input <= Number.MAX_SAFE_INTEGER &&
-        input >= Number.MIN_SAFE_INTEGER
-      ) {
-        return encode(Number(input));
-      } else {
-        return encode(String(input));
-      }
-    } else {
-      return null;
-    }
-  },
-  decode(data: Uint8Array): bigint {
-    const val = decode(data);
-    if (!(typeof val === 'string' || typeof val === 'number')) {
-      throw new DecodeError(`unexpected BigInt source: ${typeof val}`);
-    }
-
-    return BigInt(val);
-  },
-});
+const packr = new Packr({
+  useRecords: false,
+  moreTypes: true,
+  bundleStrings: true,
+  useTimestamp32: false,
+  useBigIntExtension: true,
+  skipValues: [undefined],
+} as Options);
 
 /**
- * Binary codec, uses [msgpack](https://www.npmjs.com/package/@msgpack/msgpack) under the hood
+ * Binary codec, uses [msgpackr](https://www.npmjs.com/package/msgpackr) under the hood
  * @type {Codec}
  */
 export const BinaryCodec: Codec = {
   toBuffer(obj) {
-    return encode(obj, { ignoreUndefined: true, extensionCodec });
+    return packr.pack(obj);
   },
   fromBuffer: (buff: Uint8Array) => {
-    const res = decode(buff, { extensionCodec });
+    const res: unknown = packr.unpack(buff);
     if (typeof res !== 'object' || res === null) {
       throw new Error('unpacked msg is not an object');
     }
