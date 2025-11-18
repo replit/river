cbbf60cfcb54582271cca06bcad93a94613d98ea
Jacky Zhao <j.zhao2k19@gmail.com>
2025-11-17 19:41:46 -0800

    Revert "use msgpackr instead of @msgpack/msgpack (#350)"
    This reverts commit b2d74549b00cde909225863a90d4794794e3fea6.

--- codec/binary.ts
+++ codec/binary.ts
@@ -1,25 +1,44 @@
-import { Options, Packr } from 'msgpackr';
+import { DecodeError, ExtensionCodec, decode, encode } from '@msgpack/msgpack';
 import { Codec } from './types';
 
-const packr = new Packr({
-  useRecords: false,
-  moreTypes: true,
-  bundleStrings: true,
-  useTimestamp32: false,
-  useBigIntExtension: true,
-  skipValues: [undefined],
-} as Options);
+const BIGINT_EXT_TYPE = 0;
+const extensionCodec = new ExtensionCodec();
+extensionCodec.register({
+  type: BIGINT_EXT_TYPE,
+  encode(input: unknown): Uint8Array | null {
+    if (typeof input === 'bigint') {
+      if (
+        input <= Number.MAX_SAFE_INTEGER &&
+        input >= Number.MIN_SAFE_INTEGER
+      ) {
+        return encode(Number(input));
+      } else {
+        return encode(String(input));
+      }
+    } else {
+      return null;
+    }
+  },
+  decode(data: Uint8Array): bigint {
+    const val = decode(data);
+    if (!(typeof val === 'string' || typeof val === 'number')) {
+      throw new DecodeError(`unexpected BigInt source: ${typeof val}`);
+    }
+
+    return BigInt(val);
+  },
+});
 
 /**
- * Binary codec, uses [msgpackr](https://www.npmjs.com/package/msgpackr) under the hood
+ * Binary codec, uses [msgpack](https://www.npmjs.com/package/@msgpack/msgpack) under the hood
  * @type {Codec}
  */
 export const BinaryCodec: Codec = {
   toBuffer(obj) {
-    return packr.pack(obj);
+    return encode(obj, { ignoreUndefined: true, extensionCodec });
   },
   fromBuffer: (buff: Uint8Array) => {
-    const res: unknown = packr.unpack(buff);
+    const res = decode(buff, { extensionCodec });
     if (typeof res !== 'object' || res === null) {
       throw new Error('unpacked msg is not an object');
     }
